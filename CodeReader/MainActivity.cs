d799e967ef0de8fe388877545e44cfe19a2e335c
PChiesa <PChiesa@users.noreply.github.com>
2017-06-28 17:43:23 -0300

    First Commit

---
+++ CodeReader/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Views;
+
+namespace CodeReader
+{
+    [Activity(Label = "Code Reader", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {            
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
+            //Toolbar will now take on default actionbar characteristics
+            SetActionBar(toolbar);
+
+            ActionBar.Title = "Code Reader";
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            var intent = new Intent(this, typeof(ServerConfiguration));
+            StartActivity(intent);
+
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
---
+++ CodeReader/ServerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CodeReader
+{
+    [Activity(Label = "Server Configuration")]
+    public class ServerConfiguration : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ServerConfiguration);
+        }
+    }
+}
